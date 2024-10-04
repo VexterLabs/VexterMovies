d653077815157c2fcbbe25b8c6dd0dc0a18b0dda
VexterLabs <28675709+VexterLabs@users.noreply.github.com>
2024-10-04 17:16:23 -0600

    Update dz-next-log.config.js

--- dz-next-log.config.js
+++ dz-next-log.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  platform: "DramaBox",
-  link: 'https://www.dramaboxapp.com',
+  platform: "VexterMovies",
+  link: 'https://vextermovies.vercel.app',
   ddUrl: "https://oapi.dingtalk.com/robot/send?access_token=7667bee5471210ebbbf0b201d20b82146b9d8a61d382315e6d6704c28d497e50",
   ddTitle: "海外【SEO】告警"
 }
