166fd09b399c3b11fe83cbefff48465d6e53aaf2
VexterLabs <28675709+VexterLabs@users.noreply.github.com>
2024-10-05 19:52:38 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -19,7 +19,7 @@ const BaseUrlObj = {
   // test: 'http://192.168.0.253:8080',
   //test: 'http://192.168.0.253:8080', // http://192.168.1.70:8080
   //staging: 'https://yfbwww.webfic.com',
-  //prod: 'https://www.webfic.com'
+  prod: 'https://webfic.com'
 }
 // ipua
 const IpUaUrlObj = {
